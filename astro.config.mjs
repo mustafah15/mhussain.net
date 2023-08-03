01bb4e81f31497ec75a69b46c6654bf08769c16c
Mustafa Hussain <mustafa.hussain93@gmail.com>
2023-08-03 18:01:24 +0200

    remove analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,29 +4,33 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/edge';
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mhussain.net",
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), sitemap()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }]],
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true
+      wrap: true,
     },
-    extendDefaultPlugins: true
-  }
+    extendDefaultPlugins: true,
+  },
 });
