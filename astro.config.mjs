3ad2ba98da21fbf7b3513f489b11d308f856fe63
Mustafa Hussain <mustafa.hussain93@gmail.com>
2023-08-03 11:10:46 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,38 +4,29 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/edge';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mhussain.net",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react(), sitemap()],
   output: 'server',
   adapter: vercel({
     analytics: true,
   }),
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
+      wrap: true
     },
-    extendDefaultPlugins: true,
-  },
+    extendDefaultPlugins: true
+  }
 });
